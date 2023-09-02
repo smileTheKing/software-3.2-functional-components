cbe0d86589e5bcec049c8abd06369be0b63cbfe7
Solomon Cammue <s.cammue@gmail.com>
2023-09-02 12:30:59 -0500

    restructing

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,18 +1,15 @@
 
-import Card from "./Card";
 import Movie from "./Movie";
 
 
 function Genre({ name, description,movieTitle1,movieTitle2 ,year1,year2}) {
   return (
-    <Card>
-      <h2>name: {name}</h2>
+    <div>
+      <h2> {name} Genre</h2>
       <h4>description: {description}</h4>
-      <h4>movieTitle1: {movieTitle1}</h4>
-      <h4>movieTitle2: {movieTitle2}</h4>
       <Movie title={movieTitle1} year={year1}  />
       <Movie  title={movieTitle2} year={year2} /> 
-    </Card>
+    </div>
   );
 }
 
