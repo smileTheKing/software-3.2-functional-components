7afc1e421935ad9b9051501f91c7a663de5e1abd
Solomon Cammue <s.cammue@gmail.com>
2023-09-02 23:22:32 -0500

    added a navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='nav'>
+        <nav className='navWrapper'>
+            <h1 className='logo'>W3 movie</h1>
+            <ul>
+                <li>Search</li>
+                <li>Library</li>
+                <li>About</li>
+            </ul>
+            <p>Login</p>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
